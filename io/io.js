021b8182d5de7108bcccbe3b43d1028882b33e1c
Phetparisouk <phetparisouk.souvannasacd@limayrac.fr>
2020-05-05 17:39:03 +0200

    add io

--- io/io.js
+++ io/io.js
@@ -1,5 +1,14 @@
+const fs = require('fs');
+
 module.exports.decodeHexFileContent = (filePath) =>  {
-    return new Promise((resolve, reject) => {
-        // To be implemented!
+
+    return new Promise((resolve) => {
+
+        let fileContent = fs.readFileSync(filePath, 'utf8');
+        let decode = Buffer.from(fileContent, 'hex').toString('utf8');
+
+        
+        resolve(decode);
+
     });
 }
