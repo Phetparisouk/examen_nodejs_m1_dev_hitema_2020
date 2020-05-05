30ee1d5154ccf14c105d754ca2c05c9dde66801a
Phetparisouk <phetparisouk.souvannasacd@limayrac.fr>
2020-05-05 17:31:59 +0200

    refixed api rest

--- api/api.js
+++ api/api.js
@@ -5,7 +5,6 @@ const peopleService = new PeopleService();
 const app = express();
 
 const bodyParser = require('body-parser');
-const HttpStatus = require('http-status-codes');
 const v1 = express.Router();
 
 // To be implemented!
@@ -16,7 +15,7 @@ app.use('/api/v1', v1);
 
 //Liste de tout les gens, utiliser la route /peoples 
 v1.get('/people', async (request, response) => {
-    const peoples = await peopleService.getPeople();
+    const peoples = await peopleService.getPeople(request.query);
     response.send(peoples);
 });
 
@@ -25,12 +24,11 @@ v1.put('/people/:id', async (request, response) => {
     const id = request.params.id;
     const people = request.body;
 
-    const is_zero = 0;
+    const is_zero = 0; // no magic number
     const isFind = people.name && people.name.length > is_zero;
 
-    if (!isFind) return response.sendStatus(HttpStatus.BAD_REQUEST);
-    const result = peopleService.updatePeople(id, people);
-    response.sendStatus(result);
+    const bad_req = 400; //no magic number
+    isFind ? response.sendStatus(peopleService.updatePeople(id, people)) : response.sendStatus(bad_req);
 });
 
 
